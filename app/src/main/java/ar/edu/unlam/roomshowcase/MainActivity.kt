e76fc95893e0766e1dce38ff6ce0f91e40442d9c
Maximiliano De Pietro <elwindwalker@gmail.com>
2021-09-21 21:53:08 -0300

    :hammer: manually inject dependencies

--- app/src/main/java/ar/edu/unlam/roomshowcase/MainActivity.kt
+++ app/src/main/java/ar/edu/unlam/roomshowcase/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.room.Room
 import ar.edu.unlam.roomshowcase.robot.Robot
+import ar.edu.unlam.roomshowcase.robot.RobotDao
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var database: AppDatabase
+    private lateinit var robotDao: RobotDao
 
     companion object {
         private fun buildDatabase(context: Context): AppDatabase {
@@ -16,28 +20,19 @@ class MainActivity : AppCompatActivity() {
                 "room-showroom-db"
             ).fallbackToDestructiveMigration().allowMainThreadQueries().build()
         }
-
-        /**
-         * Construirá/entablará la conexión con la base de datos, insertará un elemento en una
-         * tabla, imprimirá en consolo los elemntos insertados, borrará el elemento insertado,
-         * y volverá a imprimir los elementos que queden en la tabla.
-         */
-        fun abmDemo(context: Context) {
-            val database = buildDatabase(context)
-            val itemDao = database.itemDao()
-            itemDao.insert(Robot(name = "2B"))
-            itemDao.delete(itemDao.getById(1L).first())
-            print("Mostrando elementos después de borrar")
-            itemDao.getAll().forEach {
-                print(it.name)
-            }
-        }
     }
 
+    /**
+     * Manual dependency injection
+     */
+    private fun injectDependencies() {
+        this.database = buildDatabase(this.applicationContext)
+        this.robotDao = this.database.itemDao()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        abmDemo(applicationContext)
+        injectDependencies()
         setContentView(R.layout.activity_main)
     }
 }
